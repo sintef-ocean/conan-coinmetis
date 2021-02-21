126df0886fc1cd34d3f3c67fb4282aeace20da1c
Joakim Haugen <joakim.haugen@gmail.com>
2021-02-21 18:18:03 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,69 @@
+[![GCC Conan](https://github.com/sintef-ocean/conan-coinmetis/workflows/GCC%20Conan/badge.svg)](https://github.com/sintef-ocean/conan-coinmetis/actions?query=workflow%3A"GCC+Conan")
+[![Clang Conan](https://github.com/sintef-ocean/conan-coinmetis/workflows/Clang%20Conan/badge.svg)](https://github.com/sintef-ocean/conan-coinmetis/actions?query=workflow%3A"Clang+Conan")
+[![MSVC Conan](https://github.com/sintef-ocean/conan-coinmetis/workflows/MSVC%20Conan/badge.svg)](https://github.com/sintef-ocean/conan-coinmetis/actions?query=workflow%3A"MSVC+Conan")
+[![Download](https://api.bintray.com/packages/sintef-ocean/conan/coinmetis%3Asintef/images/download.svg)](https://bintray.com/sintef-ocean/conan/coinmetis%3Asintef/_latestVersion)
+
+
+[Conan.io](https://conan.io) recipe for [metis](http://glaros.dtc.umn.edu/gkhome/metis/metis/overview).
+
+This recipe is made with the help of `coin-or` builder repository [ThirdParty-metis](https://github.com/coin-or-tools/ThirdParty-Metis).
+The recipe generates library packages, which can be found at [Bintray](https://bintray.com/sintef-ocean/conan/coinmetis%3Asintef).
+The package is usually consumed using the `conan install` command or a *conanfile.txt*.
+
+## How to use this package
+
+1. Add remote to conan's package [registry.txt](http://docs.conan.io/en/latest/reference/config_files/registry.txt.html):
+
+   ```bash
+   $ conan remote add sintef https://api.bintray.com/conan/sintef-ocean/conan
+   ```
+
+2. Using *conanfile.txt* in your project with *cmake*
+
+   Add a [*conanfile.txt*](http://docs.conan.io/en/latest/reference/conanfile_txt.html) to your project. This file describes dependencies and your configuration of choice, e.g.:
+
+   ```
+   [requires]
+   coinmetis/[>=4.0.3]@sintef/stable
+
+   [options]
+   coinmetis:shared=False
+
+   [imports]
+   licenses, * -> ./licenses @ folder=True
+
+   [generators]
+   cmake_paths
+   cmake_find_package
+   ```
+
+   Insert into your *CMakeLists.txt* something like the following lines:
+   ```cmake
+   cmake_minimum_required(VERSION 3.13)
+   project(TheProject CXX)
+
+   include(${CMAKE_BINARY_DIR}/conan_paths.cmake)
+   find_package(coinmetis MODULE REQUIRED)
+
+   add_executable(the_executor code.cpp)
+   target_link_libraries(the_executor coinmetis::coinmetis)
+   ```
+   Then, do
+   ```bash
+   $ mkdir build && cd build
+   $ conan install .. -s build_type=<build_type>
+   ```
+   where `<build_type>` is e.g. `Debug` or `Release`.
+   You can now continue with the usual dance with cmake commands for configuration and compilation. For details on how to use conan, please consult [Conan.io docs](http://docs.conan.io/en/latest/)
+
+## Package options
+
+Option | Default | Values
+---|---|---
+shared  | True | [True, False]
+fPIC | True | [True, False]
+
+## Known recipe issues
+
+- This packages is built with the help of `msys2` and `automake`.
+- The package does not include a `test_package` and may not work as intended.
---
+++ conanfile.py
@@ -0,0 +1,105 @@
+from conans import AutoToolsBuildEnvironment, ConanFile, tools
+from contextlib import contextmanager
+from os import path, unlink
+
+
+class CoinMetisConan(ConanFile):
+    name = "coinmetis"
+    version = "4.0.3"
+    license = ("https://github.com/CIBC-Internal/metis-4.0.3/blob/master/LICENSE",)
+    author = "SINTEF Ocean"
+    url = "https://github.com/sintef-ocean/conan-coinmetis"
+    homepage = "http://glaros.dtc.umn.edu/gkhome/metis/metis/overview"
+    description =\
+        "METIS is a set of serial programs for partitioning graphs, "\
+        "partitioning finite element meshes, and producing fill " \
+        "reducing orderings for sparse matrices."
+    topics = ("Matrix ordering", "Partitioning graphs", "COIN-OR")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = {"shared": True, "fPIC": True}
+
+    _coin_helper = "ThirdParty-Metis"
+    _coin_helper_branch = "stable/2.0"
+    _autotools = None
+
+    def _configure_autotools(self):
+        if self._autotools:
+            return self._autotools
+
+        self._autotools = AutoToolsBuildEnvironment(
+            self,
+            win_bash=tools.os_info.is_windows)
+        self._autotools.libs = []
+
+        yes_no = lambda v: "yes" if v else "no"
+        configure_args = [
+            "--enable-shared={}".format(yes_no(self.options.shared)),
+            "--enable-static={}".format(yes_no(not self.options.shared)),
+        ]
+
+        if self.settings.compiler == "Visual Studio":
+            configure_args.append(
+                "--enable-msvc={}".format(self.settings.compiler.runtime))
+
+        self._autotools.configure(args=configure_args)
+        return self._autotools
+
+    @contextmanager
+    def _build_context(self):
+        if self.settings.compiler == "Visual Studio":
+            with tools.vcvars(self.settings):
+                env = {
+                    "CC": "{} cl -nologo".format(tools.unix_path(
+                        self.deps_user_info["automake"].compile)),
+                    "CXX": "{} cl -nologo".format(tools.unix_path(
+                        self.deps_user_info["automake"].compile)),
+                    "LD": "link -nologo",
+                    "AR": "{} lib".format(tools.unix_path(
+                        self.deps_user_info["automake"].ar_lib)),
+                }
+                with tools.environment_append(env):
+                    yield
+        else:
+            yield
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def build_requirements(self):
+        if tools.os_info.is_windows and not tools.get_env("CONAN_BASH_PATH"):
+            self.build_requires("msys2/20200517")
+        if self.settings.compiler == "Visual Studio":
+            self.build_requires("automake/[>=1.16.3]")
+
+    def source(self):
+        _git = tools.Git()
+        _git.clone("https://github.com/coin-or-tools/{}.git"
+                   .format(self._coin_helper),
+                   branch=self._coin_helper_branch,
+                   shallow=True)
+
+        self.run("./get.Metis", win_bash=tools.os_info.is_windows)
+
+    def build(self):
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.make()
+
+    def package(self):
+        with self._build_context():
+            autotools = self._configure_autotools()
+            autotools.install()
+
+        tools.rmdir(path.join(self.package_folder, "lib", "pkgconfig"))
+        unlink(path.join(self.package_folder, "lib", "libcoinmetis.la"))
+        self.copy("INSTALL.Metis", dst="licenses")
+
+    def package_info(self):
+        self.cpp_info.libs = ["coinmetis"]
+        # TODO: The dll is named coinmetis-2, should I be worried?
+        self.cpp_info.includedirs = [path.join("include", "coin-or", "metis")]
+
+    def imports(self):
+        self.copy("license*", dst="licenses", folder=True, ignore_case=True)
